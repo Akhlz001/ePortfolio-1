0d8d845def83dcbeef32c925d7b321d2a6b8d1e7
DerekPeacock <derek.peacock@yahoo.com>
2019-02-11 11:35:00 +0000

    Added home view of skills

--- ePortfolio/Controllers/HomeController.cs
+++ ePortfolio/Controllers/HomeController.cs
@@ -1,18 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ePortfolio.Models;
+using ePortfolio.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace ePortfolio.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Data.ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Skills()
+        {
+            ViewData["Message"] = "Your skills description page.";
+
+            return View(await _context.Skills.OrderBy(s => s.SkillNo).ToListAsync());
+        }
+
+        public async Task<IActionResult> FullSkills()
+        {
+            ViewData["Message"] = "Your skills description page.";
+
+            return View(await _context.Skills.OrderBy(s => s.SkillNo).ToListAsync());
+        }
+
+        // GET: Skills/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills
+                .FirstOrDefaultAsync(m => m.SkillID == id);
+
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return View(skill);
         }
 
         public IActionResult About()
